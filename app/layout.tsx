a8999a4c4cfb9fba006c502c43809c7dd17f17cd
LModipane <modipanesh@gmail.com>
2026-04-10 08:18:36 +0200

    refactor: improve code formatting and update portfolio description

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,31 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+	variable: '--font-geist-sans',
+	subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+	variable: '--font-geist-mono',
+	subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Lesedi Shaun Modipane Portfolio",
-  description: "A portfolio website showcasing the projects and skills of Shaun Modipane, a software developer specializing in web development and design.",
+	title: 'Lesedi Shaun Modipane Portfolio',
+	description:
+		'Welcome to my digital repository. A curated database of my professional journey, blogs, and projects timeline. This space serves as a transparent look into my thinking process and current projects, designed for those who want to explore my work and follow my progress in real-time.',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+			<body className="min-h-full flex flex-col">{children}</body>
+		</html>
+	);
 }
