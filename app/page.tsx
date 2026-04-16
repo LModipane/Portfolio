735e6edd3ef30a3cc51cb06e9baf4f54709a30bd
LModipane <modipanesh@gmail.com>
2026-04-16 18:55:56 +0200

    feat: add Download icon to resume button and improve styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-import { Mail, Sparkles } from 'lucide-react';
+import { Download, Mail, Sparkles } from 'lucide-react';
 import Image from 'next/image';
 import linkinlogo from '../public/linkedin.svg';
 import gitHubLogo from '../public/github.svg';
 
 export default function Home() {
 	return (
-		<main className="h-full w-full bg-gray-100 text-black overflow-scroll">
+		<main className="h-full w-full bg-gray-100 text-black overflow-y-scroll">
 			{/* Hero section */}
 			<header className="h-full w-full flex items-center justify-center">
 				<div className="flex flex-col items-center border-2 border-gray-300/40 rounded-lg p-8 shadow-lg">
 					{/* Availability Tag */}
-					<div className="rounded-full p-0.5 bg-linear-to-r from-blue-500 to-purple-600 mb-2 animate-gradient">
+					<div className="rounded-full p-0.5 bg-linear-to-r from-blue-500 to-purple-600 mb-2 animate-gradient lowercase animate-pulse duration-300">
 						<div className="bg-gray-100 rounded-full p-2">
 							<div className="flex items-center text-md font-extralight border-2 rounded-full bg-linear-to-r from-blue-500 to-purple-600 text-transparent bg-clip-text animate-gradient">
 								<Sparkles className="mr-3 text-blue-500" size={18} />
@@ -35,8 +35,8 @@ export default function Home() {
 						<button className="flex px-6 py-3 bg-black text-white rounded-3xl hover:bg-gray-800 transition-colors duration-300 cursor-pointer">
 							Let&apos;s Connect <Mail className="ml-2" size={20} />
 						</button>
-						<button className="ml-4 px-6 py-3 bg-gray-100 text-black border-2 border-black rounded-3xl hover:bg-gray-50 transition-colors duration-300 cursor-pointer">
-							Download Resume
+						<button className="flex ml-4 px-6 py-3 bg-gray-100 text-black border-2 border-black rounded-3xl hover:bg-gray-50 transition-colors duration-300 cursor-pointer">
+							Download Resume <Download className="ml-2" size={20} />
 						</button>
 					</div>
 
