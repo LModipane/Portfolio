1c75eb4a25b2c60585fc55fad9111f0c8873bf12
LModipane <modipanesh@gmail.com>
2026-04-17 08:54:13 +0200

    feat: add typing animation for titles and enhance About Me section

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,15 @@ import { Download, Mail, Sparkles } from 'lucide-react';
 import Image from 'next/image';
 import linkinlogo from '../public/linkedin.svg';
 import gitHubLogo from '../public/github.svg';
+import { Titles } from '@/components';
 
 export default function Home() {
+	// Note: Add Typing animation to display multiple of titles/Adjectives that describe me as a software developer, cloud Engineerand Data scientist.
 	return (
 		<main className="h-full w-full bg-gray-100 text-black overflow-y-scroll">
 			{/* Hero section */}
-			<header className="h-full w-full flex items-center justify-center">
-				<div className="flex flex-col items-center border-2 border-gray-300/40 rounded-lg p-8 shadow-lg">
+			<header className="h-full w-full flex items-center justify-center bg-white">
+				<div className="flex flex-col items-center border-2 border-gray-300/40 rounded-lg px-2 py-8 shadow-lg min-w-[93%] max-w-[95%]">
 					{/* Availability Tag */}
 					<div className="rounded-full p-0.5 bg-linear-to-r from-blue-500 to-purple-600 mb-2 animate-gradient lowercase animate-pulse duration-300">
 						<div className="bg-gray-100 rounded-full p-2">
@@ -20,12 +22,12 @@ export default function Home() {
 					</div>
 
 					{/* Call to Action */}
-					<div className=" flex flex-col justify-center items-center p-8">
-						<h1 className="text-4xl font-semibold">
-							Shaun Lesedi Modipane Execellent Software developer
+					<div className=" flex flex-col justify-center mx-auto px-2 py-8 w-full">
+						<h1 className="text-5xl font-semibold ">
+							Shaun Lesedi Modipane <Titles />
 						</h1>
-						<h2 className="text-2xl font-light mt-4 text-gray-700/90 text-center max-w-225 capitalize">
-							Ready to bring technical excellence to your next big project. Let&apos;s connect to
+						<h2 className="text-3xl font-light mt-4 text-gray-700/80 text-center ">
+							Ready to bring technical talent to your next big project. Let&apos;s connect to
 							discuss how we can build something great together.
 						</h2>
 					</div>
@@ -35,7 +37,7 @@ export default function Home() {
 						<button className="flex px-6 py-3 bg-black text-white rounded-3xl hover:bg-gray-800 transition-colors duration-300 cursor-pointer">
 							Let&apos;s Connect <Mail className="ml-2" size={20} />
 						</button>
-						<button className="flex ml-4 px-6 py-3 bg-gray-100 text-black border-2 border-black rounded-3xl hover:bg-gray-50 transition-colors duration-300 cursor-pointer">
+						<button className="flex ml-4 px-6 py-3 bg-white text-black border-2 border-black rounded-3xl hover:bg-gray-50 transition-colors duration-300 cursor-pointer">
 							Download Resume <Download className="ml-2" size={20} />
 						</button>
 					</div>
@@ -59,6 +61,27 @@ export default function Home() {
 					</div>
 				</div>
 			</header>
+			{/* About me Section */}
+			<section className="h-full w-full flex items-center justify-center bg-white">
+				<div className="flex flex-col items-center border-2 border-gray-300/40 rounded-lg p-8 shadow-lg mt-10">
+					<h2 className="text-3xl font-semibold mb-4">About Me</h2>
+					<p className="text-lg font-light text-gray-700/90 text-center max-w-225">
+						I am a passionate software developer with a strong background in cloud engineering and
+						data science. With a keen eye for detail and a commitment to excellence, I strive to
+						create innovative solutions that drive business success. My expertise spans across
+						various programming languages and technologies, allowing me to adapt and thrive in
+						dynamic environments. I am dedicated to continuous learning and growth, always seeking
+						new challenges to expand my skill set and contribute to impactful projects.
+					</p>
+				</div>
+			</section>
+			<footer className="h-full w-full flex flex-col items-center justify-center  p-8 bg-black text-white">
+				<h3>Let&apos;s Connect and build Something Great Together</h3>
+				{/* Display Email and Phone Number */}
+				<p className="text-gray-700/90 text-center">
+					&copy; {new Date().getFullYear()} Shaun Lesedi Modipane. All rights reserved.
+				</p>
+			</footer>
 		</main>
 	);
 }
---
+++ components/Titles/index.tsx
@@ -0,0 +1,28 @@
+'use client';
+import { Typewriter } from 'react-simple-typewriter';
+
+const Titles = () => {
+	return (
+		<span className="mt-2 rounded-lg p-2 shadow-lg font-light text-center">
+			<span className="bg-linear-to-r from-blue-500 to-purple-600 text-transparent bg-clip-text animate-gradient">
+				<Typewriter
+					words={[
+						'Excellent Software Developer',
+						'Experienced Cloud Engineer',
+						'Exceptional Data Scientist',
+					]}
+					loop={0}
+					cursor={false} // disable built-in cursor
+					typeSpeed={70}
+					deleteSpeed={20}
+                    delaySpeed={1000}
+				/>
+			</span>
+            <span className="ml-1 text-purple-500 animate-[blink-fast_0.4s_steps(1)_infinite]">
+                {"_"}
+            </span>
+		</span>
+	);
+};
+
+export default Titles;
